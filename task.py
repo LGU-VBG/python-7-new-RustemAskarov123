01d25713f06ee3fb23ebae931d889186bf7b812a
RustemAskarov123 <askarovrustem131@gmail.com>
2025-06-22 03:28:52 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,1 +1,8 @@
-
+def merge_lists_to_dict(keys,values):
+    return dict(zip(keys,values))
+values = [1,2,3,4]
+keys = [5,6,7,8]
+result_one = merge_lists_to_dict(keys=keys,values=values)
+result_two = merge_lists_to_dict(keys,values=values)
+print(result_one)
+print(result_two)
